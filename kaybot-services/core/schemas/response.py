00cc55cec4b792d5f90e3d1f8550a8ae495d80a9
Caleb Adepitan <calebpitan@gmail.com>
2023-08-30 10:51:10 +0100

    update: implement response serialization classes

---
+++ kaybot-services/core/schemas/response.py
@@ -0,0 +1,56 @@
+# Standard Library
+import re
+from typing import Any, Generic, Tuple, Type, TypeVar
+
+# Third Party
+from pydantic import BaseModel, computed_field
+
+# First Party
+from core.exceptions.code import ErrorCode
+from core.exceptions.http import AppHTTPException
+
+DataT = TypeVar("DataT", bound=BaseModel)
+ErrorT = TypeVar("ErrorT", bound=AppHTTPException)
+
+
+class ResponseBase(BaseModel, Generic[DataT]):
+    data: DataT
+
+    @classmethod
+    def model_parametrized_name(cls, params: Tuple[Type[Any], ...]) -> str:
+        return f"{params[0].__name__}Response"
+
+    @computed_field
+    @property
+    def typename(self) -> str:
+        type_name = type(self).__name__
+
+        return re.sub(r"(?:Response)$", "", type_name)
+
+
+class StatusResponse(ResponseBase[DataT], Generic[DataT]):
+    message: str = "Completed successfully 😁"
+    success: bool = True
+
+
+class StandardResponse(ResponseBase[DataT], Generic[DataT]):
+    ...
+
+
+class ErrorAttributes(BaseModel):
+    context: dict[str, Any] | None
+    path: tuple[str, ...]
+    message: str
+    value: Any
+
+
+class ErrorInfo(BaseModel):
+    code: ErrorCode
+    errors: list[ErrorAttributes]
+
+
+class ErrorResponse(StatusResponse[Any], Generic[ErrorT]):
+    data: None = None
+    success: bool = False
+    message: str = "Oops, failed to complete request 😔"
+    info: ErrorInfo
