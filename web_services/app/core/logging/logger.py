d1e72b9d18ae1d42b01e087103783f7b45f6997f
Caleb Adepitan <calebpitan@gmail.com>
2023-09-05 20:51:59 +0100

    chore: update logger

--- web_services/app/core/logging/logger.py
+++ web_services/app/core/logging/logger.py
@@ -17,24 +17,25 @@ class AppLogger(object):
     _console_handler = logging.StreamHandler(sys.stdout)
 
     _formatter = logging.Formatter(
-        fmt="%(log_color)s%(asctime)s.%(msecs)03dZ - %(levelname)s [%(name)s (%(process)d)] %(message)s",
+        fmt="%(asctime)s,%(msecs)03d - %(levelname)s (%(process)d) [%(name)s:%(lineno)d] %(message)s",
         datefmt="%Y-%m-%dT%H:%M:%S",
     )
 
     _colored_formatter = ColoredFormatter(
-        fmt=_formatter._fmt,
+        fmt="%(log_color)s{}".format(_formatter._fmt),
         datefmt=_formatter.datefmt,
-        # log_colors={
-        #     "DEBUG": "cyan",
-        #     "INFO": "green",
-        #     "WARNING": "yellow",
-        #     "ERROR": "red",
-        #     "CRITICAL": "red,bg_yellow",
-        # },
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_yellow",
+        },
     )
 
     def __init__(self, context: str, level: int = logging.INFO) -> None:
-        self._context = context
+        self.context = context
+        self.level = level
         self._logger = logging.getLogger(context)
 
         self._logger.propagate = False
@@ -49,7 +50,7 @@ class AppLogger(object):
         return self._logger
 
 
-def get_app_logger(context: str, level: int = logging.INFO) -> logging.Logger:
+def get_app_logger(context: str, level: int = logging.DEBUG) -> logging.Logger:
     app_logger = AppLogger(context=context, level=level)
     logger = app_logger.get_logger()
 
