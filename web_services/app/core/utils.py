caf622fd8cd5202f1920393c7c65e5d83cb2c231
Caleb Adepitan <calebpitan@gmail.com>
2023-09-10 13:22:39 +0100

    update: add util functions

--- web_services/app/core/utils.py
+++ web_services/app/core/utils.py
@@ -1,7 +1,7 @@
 # Standard Library
 from contextlib import contextmanager
 from datetime import datetime
-from typing import Any, Iterator
+from typing import TYPE_CHECKING, Any, Callable, Iterator, TypeVar
 from uuid import UUID
 
 # Third Party
@@ -11,14 +11,18 @@ from pydantic import ValidationError
 # First Party
 from app.core.authentication.token import prefix_sub
 
+if TYPE_CHECKING:
+    # First Party
+    from app.core.schemas.base import BaseModel
+
+ModelT = TypeVar("ModelT", bound="BaseModel")
+
+iso_format_date: Callable[[datetime], str] = lambda x: x.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
 
 def get_utc_time():
     """Get the current UTC time with timezone information"""
     return datetime.now(tz=pytz.utc)
-
-
-def create_error(message: str):
-    return {"message": message}
 
 
 def is_recursion_validation_error(exc: ValidationError) -> bool:
@@ -35,5 +39,31 @@ def suppress_recursion_validation_error() -> Iterator[None]:
             raise exc
 
 
-def tok_payload(account_id: UUID) -> dict[str, Any]:
-    return {"sub": prefix_sub(account_id.hex)}
+def tok_payload(
+    sub: UUID,
+    *,
+    iss: Any | None = None,
+    aud: Any | None = None,
+    **extras: Any,
+) -> dict[str, Any]:
+    payload = {"sub": prefix_sub(sub.hex)}
+
+    if iss is not None:
+        payload.update({"iss": iss})
+    if aud is not None:
+        payload.update({"aud": aud})
+
+    for key, value in extras.items():
+        if value is not None:
+            payload.update({key: value})
+
+    return payload
+
+
+def describe_field(model: type[ModelT], example: str = "(e.g, id:1754)") -> str:
+    columns = model.__fields__.keys()
+    return (", ".join(columns) + " " + example).strip()
+
+
+def split_field(sort: str, delimiter: str = ","):
+    return filter(lambda v: bool(v), sort.split(delimiter))
