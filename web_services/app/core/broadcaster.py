909a08b8059590ffe11a70e1eec7926d7f6305df
Caleb Adepitan <calebpitan@gmail.com>
2023-09-17 00:14:02 +0100

    update: remove broadcaster

--- web_services/app/core/broadcaster.py
+++
@@ -1,7 +0,0 @@
-# Third Party
-from broadcaster import Broadcast  # type: ignore
-
-# First Party
-from app.core.settings import settings
-
-broadcast = Broadcast(settings.REDIS_URL.unicode_string())
