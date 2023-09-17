eb8703447e7d80cf2abdabb46023e2ddbf2197ad
Caleb Adepitan <calebpitan@gmail.com>
2023-09-17 17:41:10 +0100

    update: minor refactorings and docstrings

--- web_services/app/core/services/conversation.py
+++ web_services/app/core/services/conversation.py
@@ -11,6 +11,7 @@ from app.core.exceptions.http import ForbiddenRequestException
 from app.core.models.conversation import Conversation as ConversationModel
 from app.core.schemas import conversation as schema
 from app.core.schemas.conversation import ConversationFilter, ConversationFilterExtra
+from app.core.schemas.conversation import ConversationUpdate
 from app.core.schemas.pagination import PageOptions
 from app.core.services.pagination import PageBuilder
 
@@ -38,12 +39,65 @@ def create_conversation_service(
     return conversation
 
 
-def get_conversation_by_id(session: Session, ctx: ServiceContext, id: UUID):
-    converstation = (
-        session.query(ConversationModel).filter(ConversationModel.id == id).one_or_none()
-    )
+def update_conversation_service(
+    session: Session,
+    ctx: ServiceContext,
+    id: UUID,
+    conversation_update: ConversationUpdate,
+):
+    """Update a conversation by a given ID
 
-    if converstation is None:
+    :param session: the database session to use to get the conversation
+
+    :param ctx: the service context necessary for running the service
+
+    :param id: the id of the conversation to be fetched
+
+    :param conversation_update: the object containing changes to be made
+
+    :raises MissingResourceException:
+        if no conversation with the specified ID is not found
+
+    :raises ForbiddenRequestException:
+        if there are not enough access rights to the conversation
+    """
+
+    conversation = get_conversation_by_id(session=session, ctx=ctx, id=id)
+    update_dict = conversation_update.model_dump(exclude_defaults=True)
+
+    # Still have this check here, even after the check in `get_conversation_by_id`,
+    # for future sake, when shared conversations can be read, but not written
+    if conversation.started_by.id != ctx.user.id:
+        raise ForbiddenRequestException("You are not allowed write access to this conversation")
+
+    for field, value in update_dict.items():
+        setattr(conversation, field, value)
+
+    # no need to `session.add` object should still be in the session
+    session.commit()
+    session.refresh(conversation)
+    return conversation
+
+
+def get_conversation_by_id(session: Session, ctx: ServiceContext, id: UUID):
+    """Get a conversation by ID
+
+    :param session: the database session to use to get the conversation
+
+    :param ctx: the service context necessary for running the service
+
+    :param id: the id of the conversation to be fetched
+
+    :raises MissingResourceException:
+        if no conversation with the specified ID is not found
+
+    :raises ForbiddenRequestException:
+        if there are not enough access rights to the conversation
+    """
+
+    conversation = session.query(ConversationModel).filter(ConversationModel.id == id).one_or_none()
+
+    if conversation is None:
         exception = MissingResourceException("Conversation not found!")
         exception.add_attributes(
             context=None,
@@ -54,10 +108,10 @@ def get_conversation_by_id(session: Session, ctx: ServiceContext, id: UUID):
         raise exception
 
     # TODO: revise when the feature to share conversations have been implemented
-    if converstation.started_by.id != ctx.user.id:
-        raise ForbiddenRequestException("You are not allowed to read this conversation")
+    if conversation.started_by.id != ctx.user.id:
+        raise ForbiddenRequestException("You are not allowed read access to this conversation")
 
-    return converstation
+    return conversation
 
 
 def get_conversations(
@@ -65,20 +119,20 @@ def get_conversations(
     session: Session,
     ctx: ServiceContext,
     filter: ConversationFilter,
-    page: PageOptions,
+    page_opts: PageOptions,
     sorts: list[str]
 ):
     pagebuilder = PageBuilder[ConversationModel, ConversationFilterExtra]()
 
-    after = page.page_cursor if page.page_forward else None
-    before = page.page_cursor if not page.page_forward else None
+    after = page_opts.page_cursor if page_opts.page_forward else None
+    before = page_opts.page_cursor if not page_opts.page_forward else None
 
     filter_extra = ConversationFilterExtra(
         started_by_id=ctx.user.id,
         **filter.model_dump(exclude_defaults=True),
     )
 
-    pages = (
+    page = (
         pagebuilder.setup(session, ConversationModel)
         .go_to_edge_after(after)
         .go_to_edge_before(before)
@@ -88,12 +142,12 @@ def get_conversations(
     )
 
     result = PagedServiceObject(
-        pages.read(page.page_size),
-        cursors=pages.cursors(),
-        page_size=pages.read_size,
-        total_pages=pages.total_size,
-        has_next=pages.has_next(),
-        has_prev=pages.has_previous(),
+        page.read(page_opts.page_size),
+        cursors=page.cursors(),
+        page_size=page.read_size,
+        total_pages=page.total_size,
+        has_next=page.has_next(),
+        has_prev=page.has_previous(),
     )
 
     return result
