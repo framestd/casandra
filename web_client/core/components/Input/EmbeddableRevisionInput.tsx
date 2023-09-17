7ba70023eb37e6b59f4edaeda4f3997ac371bb28
Caleb Adepitan <calebpitan@gmail.com>
2023-09-17 20:10:32 +0100

    update: add component

---
+++ web_client/core/components/Input/EmbeddableRevisionInput.tsx
@@ -0,0 +1,96 @@
+import { ChangeEvent, useState } from 'react';
+
+import {
+  forwardRef,
+  Icon,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputLeftElementProps,
+  InputProps,
+  InputRightElement,
+  useColorModeValue,
+} from '@/chakra-ui/react';
+
+import { IconType } from 'react-icons';
+import { IoArrowUndoOutline } from 'react-icons/io5';
+import { MdOutlineFileDownloadDone } from 'react-icons/md';
+
+export interface EmbeddableProps extends InputProps {
+  _ps?: InputProps['ps'];
+  _height: string;
+  _value: string;
+  _LeftIcon?: IconType;
+  _LeftIconProps?: InputLeftElementProps;
+  onRestore?: () => void;
+  onRevise?: (changes: string) => void;
+}
+
+export const EmbeddableRevisionInput = forwardRef<EmbeddableProps, 'input'>((props: EmbeddableProps, ref) => {
+  const { _ps, _height, _value, _LeftIconProps, _LeftIcon, onRestore, onRevise, ...rest } = props;
+
+  const colorScheme = useColorModeValue('blackAlpha', 'whiteAlpha');
+  const [changes, setChanges] = useState(() => _value);
+
+  const isDisabled = changes === _value || changes.trim() === '';
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => setChanges(e.target.value);
+  const restore = () => {
+    setChanges(_value);
+    onRestore?.();
+  };
+
+  const handleRevision = () => onRevise?.(changes.trim());
+
+  return (
+    <InputGroup size="sm" height={_height} sx={{ '--input-height': _height }}>
+      <InputLeftElement {..._LeftIconProps} width={_ps}>
+        <Icon as={_LeftIcon} fontSize="lg" />
+      </InputLeftElement>
+
+      <Input
+        autoFocus={true}
+        borderColor="transparent"
+        focusBorderColor="transparent"
+        fontWeight="600"
+        value={changes}
+        onChange={handleChange}
+        sx={{ ps: _ps, pe: 16, '--input-height': _height }}
+        ref={ref}
+        {...rest}
+      />
+
+      <InputRightElement width={16}>
+        <IconButton
+          size="sm"
+          aria-label="Edit conversation title"
+          variant="ghost"
+          borderRadius="full"
+          colorScheme={colorScheme}
+          color="currentcolor"
+          bgColor="transparent"
+          icon={<Icon as={IoArrowUndoOutline} fontSize="md" />}
+          _hover={{ bgColor: 'transparent' }}
+          _active={{ bgColor: 'transparent' }}
+          onClick={restore}
+        />
+
+        <IconButton
+          size="sm"
+          aria-label="Save"
+          variant="ghost"
+          borderRadius="full"
+          colorScheme={colorScheme}
+          color="currentcolor"
+          bgColor="transparent"
+          icon={<Icon as={MdOutlineFileDownloadDone} fontSize="lg" />}
+          _hover={{ bgColor: 'transparent' }}
+          _active={{ bgColor: 'transparent' }}
+          isDisabled={isDisabled}
+          onClick={handleRevision}
+        />
+      </InputRightElement>
+    </InputGroup>
+  );
+});
