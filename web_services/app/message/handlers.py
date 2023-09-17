060c2258df9dfbe63ba82e6dfd1f388fbd037cde
Caleb Adepitan <calebpitan@gmail.com>
2023-09-17 17:38:43 +0100

    update router handlers
    - remove cookie/token creation from message publishing
    - implement endpoint to retrieve messages

--- web_services/app/message/handlers.py
+++ web_services/app/message/handlers.py
@@ -1,5 +1,4 @@
 # Standard Library
-from datetime import UTC, datetime, timedelta
 from typing import Annotated, cast
 
 # Third Party
@@ -8,16 +7,16 @@ from pydantic import UUID4
 from sqlalchemy.orm import Session
 
 # First Party
-from app.core.authentication.token import JWTRS256Token
 from app.core.deps import ServiceContext, get_db, get_service_context
+from app.core.deps import preprocess_sort_param
 from app.core.logging.logger import get_app_logger
-from app.core.schemas.message import Message, MessageCreate
-from app.core.schemas.response import StandardPaginatedResponse, StandardResponse
-from app.core.schemas.response import StatusResponse
-from app.core.services.message import get_chat_message_by_id, handle_message
-from app.core.settings import settings
+from app.core.schemas.message import Message, MessageCreate, MessageFilter
+from app.core.schemas.pagination import PageInfo, PageOptions
+from app.core.schemas.response import ResponseMetadata, StandardPaginatedResponse
+from app.core.schemas.response import StandardResponse, StatusResponse
+from app.core.services.message import get_message_by_id
+from app.core.services.message import get_messages_by_conversation_id, handle_message
 from app.core.specs.additional_responses import responses
-from app.core.utils import tok_payload
 
 # Local Folder
 from .router import router
@@ -45,20 +44,6 @@ async def publish_message(
 
     chat_message = await handle_message(session=db, ctx=ctx, message=message)
 
-    expiry = timedelta(minutes=60)
-
-    websocket_access_token = JWTRS256Token.from_data(
-        data=tok_payload(ctx.account.id),
-        expires_delta=expiry,
-    )
-
-    response.set_cookie(
-        key=settings.WS_ACCESS_TOKEN_KEY,
-        value=str(websocket_access_token),
-        expires=(datetime.utcnow() + expiry).astimezone(tz=UTC),
-        path="/conversations",
-    )
-
     return StatusResponse[Message](
         data=cast(Message, chat_message),
         message="Message understood",
@@ -82,7 +67,7 @@ def read_chat_message_by_id(
 ):
     """Read a message by a given id."""
 
-    chat_message = get_chat_message_by_id(session=db, ctx=ctx, id=id)
+    chat_message = get_message_by_id(session=db, ctx=ctx, id=id)
 
     response = StandardResponse[Message](data=cast(Message, chat_message))
 
@@ -99,8 +84,37 @@ def read_chat_message_by_id(
     },
 )
 def read_chat_messages_by_conversation_id(
+    *,
     conversation_id: UUID4,
+    sort: Annotated[list[str], Depends(preprocess_sort_param(Message))],
+    filter: Annotated[MessageFilter, Depends()],
+    page: Annotated[PageOptions, Depends()],
     ctx: Annotated[ServiceContext, Depends(get_service_context)],
     db: Annotated[Session, Depends(get_db)],
 ):
-    pass
+    message = get_messages_by_conversation_id(
+        session=db,
+        ctx=ctx,
+        conversation_id=conversation_id,
+        filter=filter,
+        sorts=sort,
+        page_opts=page,
+    )
+
+    start_cursor, end_cursor = message.cursors
+    has_prev, has_next = message.has_prev, message.has_next
+
+    response = StandardPaginatedResponse(
+        data=cast(list[Message], message.obj),
+        metadata=ResponseMetadata(
+            total_objects=message.total_pages,
+            page_info=PageInfo(
+                top_cursor=start_cursor,
+                bottom_cursor=end_cursor,
+                has_prev=has_prev,
+                has_next=has_next,
+            ),
+        ),
+    )
+
+    return response
