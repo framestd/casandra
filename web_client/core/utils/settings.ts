57ac6db65bcd07dbbbd3d4a3b93b33e1b46a1256
Caleb Adepitan <calebpitan@gmail.com>
2023-09-22 20:49:39 +0100

    update: define server base path (e.g, `/api/v1/`)

--- web_client/core/utils/settings.ts
+++ web_client/core/utils/settings.ts
@@ -1,5 +1,6 @@
 export const APP_NAME = process.env.APP_NAME!;
 
+export const SERVER_BASE_PATH = '';
 export const SERVER_BASE_URL = process.env.SERVER_BASE_URL!;
 export const SERVER_INTERNAL_ADDRESS = process.env.SERVER_INTERNAL_ADDRESS!;
 
