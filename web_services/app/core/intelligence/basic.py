19b50324e4075bbbe0e735f56b1c0414a0a35c76
Caleb Adepitan <calebpitan@gmail.com>
2023-09-29 17:26:09 +0100

    Update prompt generation

--- web_services/app/core/intelligence/basic.py
+++ web_services/app/core/intelligence/basic.py
@@ -1,11 +1,40 @@
 # Standard Library
-from typing import Any, Generator, cast
+from typing import Any, Generator, Literal, TypedDict, cast
 
 # Third Party
 import openai
+from openai.openai_object import OpenAIObject
 
 # First Party
+from app.core.logging.logger import get_app_logger
+from app.core.models.message import ConversationMessageRoleEnum
 from app.core.schemas.openai import ChatCompletionResponseStream
+
+logger = get_app_logger(__name__)
+
+METADATA_PROMPT = """Reply to the following message from a user and using the markdown front matter format, you'll suggest a topic and generate a short description of not more than 15 words for the ensuing conversation. Then go ahead and respond to the message after suggesting a topic and description
+###
+The desired format for suggesting a topic and description is given below:
+---
+topic: <YOUR SUGGESTED TOPIC>
+description: <YOUR SUGGESTED DESCRIPTION>
+---
+###
+<YOUR REPLY HERE>
+###
+
+Message:
+"""
+
+
+class OpenAIMessagePromptDict(TypedDict):
+    role: Literal["user", "assistant"]
+    content: str
+
+
+class OpenAISystemPromptDict(TypedDict):
+    role: Literal["system"]
+    content: str
 
 
 class Prompt(object):
@@ -17,19 +46,32 @@ class Prompt(object):
         self.user = user
 
 
-def generate_prompt(question: str) -> Prompt:
+def create_openai_message_prompt(
+    role: ConversationMessageRoleEnum, body: str
+) -> OpenAIMessagePromptDict:
+    return OpenAIMessagePromptDict(
+        role="user" if role is ConversationMessageRoleEnum.human else "assistant",
+        content=body,
+    )
+
+
+def create_aggregate_prompts(
+    *, prompts: list[OpenAIMessagePromptDict]
+) -> tuple[OpenAISystemPromptDict, list[OpenAIMessagePromptDict]]:
     """Generate a prompt from a give question or message,
     a background information for the personality which is predefined
     and set by an enum of `PersonalityBackground`
 
     """
-    system = f"You are a savage assistant, with great humor and sarcasm"
 
-    return Prompt(system=system, user=question)
+    system = f"You are a savage assistant, with great humor and sarcasm."
+
+    return (OpenAISystemPromptDict(role="system", content=system), prompts)
 
 
-def generate_response(
-    prompt: Prompt,
+def generate_completion_stream(
+    aggregate_prompts: tuple[OpenAISystemPromptDict, list[OpenAIMessagePromptDict]],
+    with_metadata: bool | None = None,
     max_tokens: int = 500,
     temperature: float = 0.5,
 ) -> Generator[ChatCompletionResponseStream, Any, None]:
@@ -42,19 +84,25 @@ def generate_response(
 
     """
 
+    system_prompt, message_prompts = aggregate_prompts
+
+    if with_metadata is True:
+        end = message_prompts[-1:][0]
+        end.update({"content": METADATA_PROMPT + " " + end.get("content")})
+
+    logger.debug(message_prompts)
+
     response = openai.ChatCompletion.create(  # type: ignore
         model="gpt-3.5-turbo",
         temperature=temperature,
         max_tokens=max_tokens,
-        messages=[
-            {"role": "system", "content": prompt.system},
-            {"role": "user", "content": prompt.user},
-        ],
+        messages=[system_prompt, *message_prompts],
         stream=True,
     )
 
     for chunk in response:  # type: ignore
-        yield cast(ChatCompletionResponseStream, chunk)
+        oai_object = cast(OpenAIObject, chunk)
+        oai_object_dict = cast(dict[str, Any], oai_object.to_dict_recursive())
 
-    # return cast(ChatCompletionResponseBody, response)
+        yield ChatCompletionResponseStream(**oai_object_dict)
     return None
