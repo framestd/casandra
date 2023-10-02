7cdd0164e2fb5bd4bbfdb5320f6fb983994fc1a4
Caleb Adepitan <calebpitan@gmail.com>
2023-10-02 21:30:58 +0100

    update: improve conversation bar UI/UX

--- web_client/core/composition/ConversationLayout/ConversationBar.tsx
+++ web_client/core/composition/ConversationLayout/ConversationBar.tsx
@@ -1,30 +1,28 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
-import { Button, Flex, Icon, IconButton, StackProps, useColorModeValue, VStack } from '@/chakra-ui/react';
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Box, StackProps, VStack } from '@/chakra-ui/react';
 
-import Link from 'next/link';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { TbMessage } from 'react-icons/tb';
-
-import { EmbeddableRevisionInput } from '@/core/components/Input';
-import { Typography } from '@/core/components/Typography';
-import { usePagedNormalizer, useThemeConstants } from '@/core/composition/hooks';
+import { Conversation } from '@/client';
+import { useGroupBy, usePagedNormalizer, useThemeConstants } from '@/core/composition/hooks';
 import { useReadConversationsService, useReviseConversationService } from '@/core/services/conversation';
-import { backdropFactory } from '@/core/theme';
-import { CONVERSATIONS } from '@/core/utils/routes';
+import { timeAgo } from '@/core/utils/date';
 
 import { ConversationBarToolbar } from './ConversationBarToolbar';
+import { ConversationList, ConversationListProps } from './ConversationList';
+import { backdropFactory } from '@/core/theme';
+import { range } from '@/core/utils';
 
 export interface ConversationBarProps extends StackProps {
   activeConversationId: string;
 }
 
 export const ConversationBar = ({ activeConversationId, ...rest }: ConversationBarProps) => {
+  const { blended_bg } = useThemeConstants();
   const [subjectFilter, setSubjectFilter] = useState<string>();
-  const { data } = useReadConversationsService({
-    variables: { sort: ['updated_at:desc'], subject: subjectFilter },
+  const { data, dataUpdatedAt } = useReadConversationsService({
+    variables: { sort: ['last_active_at:desc'], subject: subjectFilter },
     select: (data) => {
       return {
         ...data,
@@ -33,124 +31,64 @@ export const ConversationBar = ({ activeConversationId, ...rest }: ConversationB
     },
   });
 
-  const messageIconColor = useColorModeValue('green.600', 'green.400');
-  const buttonColorScheme = useColorModeValue('blackAlpha', 'whiteAlpha');
-  const textOverflowShade = useColorModeValue('#ecf0f0', '#141414');
-  const textOverflowShadeActive = useColorModeValue('#dadfdf', '#262626');
-  const textOverflowShadeHover = useColorModeValue('#dce0e1', '#222222');
-  const { blended_c, blended_bg, blended_active_bg, blended_hover_bg } = useThemeConstants();
-
   const conversations = usePagedNormalizer(data?.pages || []);
-  const [idOfConversationToEdit, setIdOfConversationToEdit] = useState<string>();
+  const grouped = useGroupBy(
+    conversations,
+    useCallback((c: Conversation) => c.last_active_at, []),
+    useCallback((v: string) => timeAgo(v), []),
+  );
 
-  const btnPx = 4;
-  const inputHeight = '2.5rem';
-  const inputPs = '3.375rem';
+  const entries = useMemo(() => Array.from(grouped.entries()), [grouped]);
+  const activeIndices = useMemo(() => Array.from(range(entries.length)), [entries.length]);
 
   const reviseConversationHandler = useReviseConversationService();
 
-  const handleRestore = () => setIdOfConversationToEdit(undefined);
-
-  const saveRevision = (changes: string) => {
-    if (!idOfConversationToEdit) return;
-    reviseConversationHandler.mutate({ id: idOfConversationToEdit, subject: changes });
-
-    handleRestore();
-  };
+  const saveRevision = useCallback(
+    (revision: Parameters<NonNullable<ConversationListProps['onReviseTopic']>>[0]) => {
+      reviseConversationHandler.mutate({ id: revision.id, subject: revision.subject });
+    },
+    [reviseConversationHandler],
+  );
 
   return (
     <VStack
       spacing={0}
-      height="full"
       width="full"
+      height="full"
+      overflowY="auto"
       borderRadius="2xl"
       alignItems="flex-start"
-      overflowY="auto"
       {...rest}
     >
       <ConversationBarToolbar filter={subjectFilter} onFilterChange={(filter) => setSubjectFilter(filter)} />
 
-      <VStack width="full" alignItems="flex-start" flex="1 1 auto" {...backdropFactory({ bgColor: blended_bg })}>
-        {conversations.map((conversation) => {
-          const isActive = activeConversationId === conversation.id;
-          const isInEditState = conversation.id === idOfConversationToEdit;
-
-          const handleEditClick = () => setIdOfConversationToEdit(conversation.id);
-
+      <Accordion
+        width="full"
+        flex="1 1 auto"
+        key={dataUpdatedAt}
+        allowMultiple={true}
+        defaultIndex={activeIndices}
+        {...backdropFactory({ bgColor: blended_bg })}
+      >
+        {entries.map(([group, conversations]) => {
           return (
-            <Flex
-              width="full"
-              alignItems="center"
-              key={conversation.id}
-              bgColor={isActive ? blended_active_bg : undefined}
-              sx={{ '--text-overflow-shade': isActive ? textOverflowShadeActive : textOverflowShade }}
-              _hover={{
-                bgColor: isActive ? undefined : blended_hover_bg,
-                '--text-overflow-shade': isActive ? undefined : textOverflowShadeHover,
-              }}
-              _active={{ bgColor: blended_active_bg }}
-            >
-              {idOfConversationToEdit !== conversation.id && (
-                <Button
-                  as={Link}
-                  href={`${CONVERSATIONS}/${conversation.id}`}
-                  borderRadius={0}
-                  colorScheme={buttonColorScheme}
-                  color="currentcolor"
-                  width="full"
-                  variant="ghost"
-                  justifyContent="flex-start"
-                  leftIcon={<Icon as={TbMessage} fontSize="lg" color={messageIconColor} />}
-                  bgColor="transparent"
-                  _hover={{ bgColor: 'transparent' }}
-                  _active={{ bgColor: 'transparent' }}
-                >
-                  <Typography
-                    ps={3}
-                    fontWeight="600"
-                    fontSize="sm"
-                    isTruncated={true}
-                    width="full"
-                    position="relative"
-                    sx={{ maskImage: `linear-gradient(90deg, var(--text-overflow-shade) 50%, transparent)` }}
-                  >
-                    {conversation.subject}
-                  </Typography>
-                </Button>
-              )}
-
-              {idOfConversationToEdit === conversation.id && (
-                <EmbeddableRevisionInput
-                  _ps={inputPs}
-                  _height={inputHeight}
-                  _value={conversation.subject}
-                  _LeftIcon={TbMessage}
-                  _LeftIconProps={{ ps: btnPx, pe: 5, color: messageIconColor }}
-                  onRestore={handleRestore}
-                  onRevise={saveRevision}
+            <AccordionItem mt={4} key={group} borderWidth={0} _first={{ borderWidth: 0 }} _last={{ borderWidth: 0 }}>
+              <h2>
+                <AccordionButton px={4} py={0} mb={2} fontSize="sm">
+                  <Box opacity={0.6}>{group}</Box>
+                </AccordionButton>
+              </h2>
+              <AccordionPanel px={0} py={0}>
+                <ConversationList
+                  conversations={conversations}
+                  activeConversationId={activeConversationId}
+                  onReviseTopic={saveRevision}
                 />
-              )}
-
-              {isActive && !isInEditState && (
-                <IconButton
-                  me={2}
-                  size="sm"
-                  aria-label="Edit Conversation"
-                  variant="ghost"
-                  borderRadius="full"
-                  colorScheme={buttonColorScheme}
-                  color={blended_c}
-                  bgColor="transparent"
-                  icon={<Icon as={AiOutlineEdit} fontSize="lg" />}
-                  _hover={{ bgColor: blended_hover_bg }}
-                  _active={{ bgColor: blended_active_bg }}
-                  onClick={handleEditClick}
-                />
-              )}
-            </Flex>
+              </AccordionPanel>
+            </AccordionItem>
           );
         })}
-      </VStack>
+      </Accordion>
     </VStack>
   );
 };
