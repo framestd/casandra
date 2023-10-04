8e03eb95856edf011752cd446843285f900afd1b
Caleb Adepitan <calebpitan@gmail.com>
2023-10-04 19:38:25 +0100

    update: imporve styling and add message quote button

--- web_client/core/components/ConversationMessage/ConversationMessage.tsx
+++ web_client/core/components/ConversationMessage/ConversationMessage.tsx
@@ -1,20 +1,24 @@
 'use client';
 
-import { Avatar, HStack, StackProps, useColorModeValue } from '@/chakra-ui/react';
+import { Avatar, HStack, Icon, IconButton, StackProps, VStack, useColorModeValue } from '@/chakra-ui/react';
 
 import { ConversationMessageRoleEnum } from '@/client';
 
 import { Markdown } from '../Markdown/Markdown';
 import { Typography } from '../Typography';
+import { IoArrowUndoOutline } from 'react-icons/io5';
 
-export interface ChatMessageProps extends StackProps {
+export interface ConversationMessageProps extends StackProps {
+  message_id: string;
   message: string;
   role: ConversationMessageRoleEnum;
   enitity: string;
   streaming?: boolean;
 }
 
-export const ConversationMesssage = ({ message, enitity, role, ...rest }: ChatMessageProps) => {
+export const messageToId = (message: string) => `message-${message.replace(/\s+/g, '-')}`
+
+export const ConversationMesssage = ({ message_id, message, enitity, role, ...rest }: ConversationMessageProps) => {
   const highlght = useColorModeValue('blackAlpha.100', 'whiteAlpha.100');
   const isAssistant = role === ConversationMessageRoleEnum.ROBOT;
   return (
@@ -23,19 +27,31 @@ export const ConversationMesssage = ({ message, enitity, role, ...rest }: ChatMe
       py={8}
       spacing={4}
       width="full"
+      id={messageToId(message_id)}
       alignItems="baseline"
       borderBottomWidth={1}
       bgColor={isAssistant ? highlght : undefined}
       {...rest}
     >
-      <Avatar name={enitity} size="sm" position="sticky" top={3} userSelect="none" />
+      <VStack spacing={4} height="full" position="sticky" top={3}>
+        <Avatar name={enitity} size="sm" userSelect="none" />
+
+        <IconButton
+          aria-label="quote"
+          size="sm"
+          colorScheme="blue"
+          variant="ghost"
+          borderRadius="full"
+          icon={<Icon as={IoArrowUndoOutline} fontSize="md" />}
+        />
+      </VStack>
 
       <Typography
         width="0"
         flex="1 1 auto"
         sx={{
           '& pre:not(:last-child)': { mb: 8 },
-          '& p:not(:last-of-type)': {
+          '& p:not(:last-child)': {
             whiteSpace: 'pre-wrap',
             width: 'full',
             mb: 8,
