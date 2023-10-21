9ae0a4f2a814ab1fdec12035785ef6f29efe3962
Caleb Adepitan <calebpitan@gmail.com>
2023-10-21 18:19:07 +0100

    update: get session and pass session object to home page component

--- web_client/app/page.tsx
+++ web_client/app/page.tsx
@@ -1,11 +1,13 @@
 import { getApplicationInfo_Cached } from '@/core/services/build-props';
 
 import { HomeScaffold } from './home';
+import { getAppServerSession } from '@/core/services/next-auth';
 
 export default async function Home() {
+  const session = await getAppServerSession()
   const res = await getApplicationInfo_Cached();
 
   const data = res.data;
 
-  return <HomeScaffold appDesc={data.description} appName={data.title} />;
+  return <HomeScaffold appDesc={data.description} appName={data.title} session={session} />;
 }
