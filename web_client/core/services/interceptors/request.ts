bc26be9d055700d78afef32867c34f5ae14c840a
Caleb Adepitan <calebpitan@gmail.com>
2023-10-21 20:05:28 +0100

    Add request interceptor
    - using request interceptor, catch requests with empty bearer token in
    their authorization header

--- web_client/core/services/interceptors/request.ts
+++ web_client/core/services/interceptors/request.ts
@@ -1,6 +1,21 @@
 import { AxiosError, AxiosRequestConfig } from 'axios';
+import { MissingAccessTokenException } from '../config';
 
+type RequestInterceptor = <R extends AxiosRequestConfig = AxiosRequestConfig>(r: R) => R | Promise<R>;
 type RequestErrorInterceptor = (e: AxiosError) => Promise<AxiosRequestConfig>;
+
+export const createRequestInterceptor = () => {
+  const requestInterceptor: RequestInterceptor = async (config) => {
+    if (config.headers && config.headers.Authorization) {
+      const auth = config.headers.Authorization as string;
+      const [type, token] = auth.split(/\s+/);
+      if (type.toLowerCase() === 'bearer' && (!token || token.trim() === ''))
+        throw new MissingAccessTokenException('No access token');
+    }
+    return config;
+  };
+  return requestInterceptor;
+};
 
 export const createRequestErrorInterceptor = () => {
   const errorInterceptor: RequestErrorInterceptor = async (err) => {
