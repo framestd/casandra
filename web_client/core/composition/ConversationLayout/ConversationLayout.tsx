c2cb46806e591a8a5f1ce1b8af174c93bffba76c
Caleb Adepitan <calebpitan@gmail.com>
2023-11-07 12:45:29 +0100

    Improve layout

--- web_client/core/composition/ConversationLayout/ConversationLayout.tsx
+++ web_client/core/composition/ConversationLayout/ConversationLayout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode, useContext } from 'react';
+import { Fragment, ReactNode, useContext, useEffect, useRef, useState } from 'react';
 
 import { Box, Flex, VStack } from '@/chakra-ui/react';
 
@@ -8,17 +8,17 @@ import { useParams } from 'next/navigation';
 import { ErrorBoundary } from 'react-error-boundary';
 
 import { AppBar } from '@/core/components/AppBar';
+import { GoogleAPIsLoader } from '@/core/components/GoogleAPIs';
+import { SplashScreen } from '@/core/components/Loader';
 import { ProfileMenu } from '@/core/components/ProfileMenu';
 import { ConfigContext } from '@/core/components/Providers';
 import { backdropFactory } from '@/core/theme';
-import { APP_BAR_HEIGHT } from '@/core/utils';
 
 import { ConversationContextProvider } from '../Conversation';
 import { useFallbackUI } from '../ErrorStates';
 import { useAppSession, useThemeConstants } from '../hooks';
 import { ConversationBar } from './ConversationBar';
 import { ConversationCustomizer } from './ConversationCustomizer';
-import { SplashScreen } from '@/core/components/Loader';
 
 export interface ChatLayoutProps {
   children?: ReactNode;
@@ -27,14 +27,25 @@ export interface ChatLayoutProps {
 export const ConversationLayout = ({ children }: ChatLayoutProps) => {
   const params = useParams();
   const session = useAppSession({ required: true });
+  const appBarRef = useRef<HTMLDivElement>(null);
   const { config } = useContext(ConfigContext);
   const { blended_bg } = useThemeConstants();
+  const [appBarRect, setAppBarRect] = useState<DOMRect | null>(null);
 
   const FallbackUI = useFallbackUI({
     width: 700,
     marginInline: 'auto',
     ...backdropFactory({ bgColor: blended_bg }),
   });
+
+  useEffect(() => {
+    const frame = window.requestAnimationFrame(() => {
+      const appBar = appBarRef.current;
+      if (!appBar) return;
+      setAppBarRect(appBar.getBoundingClientRect());
+    });
+    return () => window.cancelAnimationFrame(frame);
+  }, []);
 
   const app_name = config.application_config.name;
 
@@ -44,41 +55,46 @@ export const ConversationLayout = ({ children }: ChatLayoutProps) => {
   const image = session.data.user.image;
 
   return (
-    <Box height="full" bg="var(--root-bg)">
-      <ConversationContextProvider>
-        <AppBar type="tool" title={app_name} />
+    <Fragment>
+      <GoogleAPIsLoader apis={['gapi', 'gis']} />
 
-        <Flex height={`calc(100% - ${APP_BAR_HEIGHT}px)`} width="full">
-          <VStack
-            px={6}
-            pb={8}
-            spacing={4}
-            flexShrink={1}
-            alignSelf="flex-end"
-            height="full"
-            width={{ base: 'auto', lg: 300 }}
-            display={{ base: 'none', lg: 'flex' }}
-          >
-            <ConversationBar activeConversationId={params.id.toString()} flex="1 1 auto" />
-
-            <ProfileMenu name={name} image={image} />
-          </VStack>
-
-          <Flex
-            px={6}
-            mx="auto"
-            justifyContent="center"
-            height="full"
-            width={{ base: 'full', md: '48em', lg: '40em', '2xl': '48em' }} // md: 768, lg: 640, 2xl: 768
-          >
-            <ErrorBoundary fallbackRender={FallbackUI}>{children}</ErrorBoundary>
-          </Flex>
-
-          <Box px={6} pb={8} width={{ base: 'auto', xl: 250, '2xl': 300 }} display={{ base: 'none', xl: 'flex' }}>
-            <ConversationCustomizer />
+      <Box height="full" bg="var(--root-bg)">
+        <ConversationContextProvider>
+          <Box p={{ lg: 2 }} ref={appBarRef}>
+            <AppBar type="tool" title={app_name} borderRadius={{ lg: '2xl' }} />
           </Box>
-        </Flex>
-      </ConversationContextProvider>
-    </Box>
+
+          <Flex height={`calc(100% - ${appBarRect?.height ?? 0}px)`} width="full">
+            <VStack
+              px={2}
+              pb={8}
+              spacing={4}
+              flexShrink={1}
+              alignSelf="flex-end"
+              height="full"
+              width={{ base: 'auto', lg: 300 }}
+              display={{ base: 'none', lg: 'flex' }}
+            >
+              <ConversationBar activeConversationId={params.id.toString()} flex="1 1 auto" />
+
+              <ProfileMenu name={name} image={image} />
+            </VStack>
+
+            <Flex
+              mx="auto"
+              justifyContent="center"
+              height="full"
+              width={{ base: 'full', md: '48em', lg: '40em', '2xl': '48em' }} // md: 768, lg: 640, 2xl: 768
+            >
+              <ErrorBoundary fallbackRender={FallbackUI}>{children}</ErrorBoundary>
+            </Flex>
+
+            <Box px={2} pb={8} width={{ base: 'auto', xl: 300 }} display={{ base: 'none', xl: 'flex' }}>
+              <ConversationCustomizer />
+            </Box>
+          </Flex>
+        </ConversationContextProvider>
+      </Box>
+    </Fragment>
   );
 };
